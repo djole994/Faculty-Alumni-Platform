28298fe0e3c7e464d44674aae8c3423a646a27d9
Djordje Radovic <120112255+djole994@users.noreply.github.com>
2026-01-03 13:35:51 +0100

    Create Country.cs

---
+++ backend/src/AlumniApi/Models/Country.cs
@@ -0,0 +1,8 @@
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string IsoCode { get; set; } 
+        public double DefaultLatitude { get; set; }
+        public double DefaultLongitude { get; set; }
+    }
