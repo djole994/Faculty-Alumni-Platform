c199d2a851fe384b7081e3d4fb5a21578b4c6cac
Djordje Radovic <120112255+djole994@users.noreply.github.com>
2026-01-12 19:07:12 +0100

    Update IEmailSender.cs

--- backend/src/AlumniApi/Services/Geocoding/IEmailSender.cs
+++ backend/src/AlumniApi/Services/Geocoding/IEmailSender.cs
@@ -1,1 +1,12 @@
+using System.Net.Mail;
+
+namespace AlumniApi.Services.Email
+{
+   
+    public interface IEmailSender
+    {
+        Task SendAsync(EmailMessage message, CancellationToken ct = default);
+    }
+
+}
 
